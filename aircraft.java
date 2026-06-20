83a1c6d19d5737653f9154225c689bd4dc5d1add
Hendric Agnote <hendricag0127@gmail.com>
2026-06-20 12:49:11 -0700

    Addition of aircraft class to add different types of aircraft.

---
+++ aircraft.java
@@ -0,0 +1,6 @@
+public class aircraft {
+    String aircraftName;
+    Int fuel;
+    
+
+}
--- informationAI.java
+++ informationAI.java
@@ -1,3 +1,12 @@
 public class informationAI {
+    String departingAirport;
+    String arrivalAirport;
+    String date;
+    int time;
+    aircraft aircraftType;
     
+
+    public informationAI(){
+
+    }
 }
